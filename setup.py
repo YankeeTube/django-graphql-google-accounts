046e19b9362d9ba735d95bbb05b00267e373346e
yankee <subyankee@gmail.com>
2021-05-14 16:37:13 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+setup(
+    name='django-graphql-google-accounts',
+    version='0.1',
+    packages=['accounts'],
+    long_description=README,
+    url='https://github.com/YankeeTube/django-graphql-google-accounts',
+    author='GM Yankee',
+    author_email='subyankee@gmail.com',
+    license='MIT',
+
+    install_requires=[
+        'django',
+        'jwt',
+        'requests',
+        'graphene',
+        'graphene_django',
+    ],
+)
